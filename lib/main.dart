4e79d048df0ed182701b545346877c6579803c7b
dkvirus <3548310748@qq.com>
2020-01-08 08:44:42 +0800

    fix: 设置状态栏背景颜色透明

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import './pages/shelf.dart';
 import './pages/classify.dart';
 import './pages/search.dart';
 
-void main() => runApp(MaterialApp(
-      title: '公羊阅读',
-      initialRoute: '/',
-      routes: {
-        '/': (BuildContext context) => ShelfPage(),
-        '/shelf': (BuildContext context) => ShelfPage(),
-        '/classify': (BuildContext context) => ClassifyPage(),
-        '/search': (BuildContext context) => SearchPage(),
-      },
-    ));
+void main() {
+  runApp(MaterialApp(
+    title: '公羊阅读',
+    initialRoute: '/',
+    routes: {
+      '/': (BuildContext context) => ShelfPage(),
+      '/shelf': (BuildContext context) => ShelfPage(),
+      '/classify': (BuildContext context) => ClassifyPage(),
+      '/search': (BuildContext context) => SearchPage(),
+    },
+  ));
+
+  // 设置状态栏背景颜色透明
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+  );
+}
